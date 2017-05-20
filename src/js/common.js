fc15cdf5dfce434ddc0bfb7f589d02f840a66ba3
artlenk.ru@gmail.com <df5ukoi7ht>
2017-05-20 21:10:07 +0300

    Added video in header. Created sound block

--- src/js/common.js
+++ src/js/common.js
@@ -1,45 +1,69 @@
 $(document).ready(function () {
-    $(function () {
-        $("a[href^='#']").click(function () {
-            var _href = $(this).attr("href");
-            $("html, body").animate({scrollTop: $(_href).offset().top + "px"});
-            return false;
-        });
+  $(function () {
+    $(".slider-range").slider({
+      range: "min",
+      value: 50,
+      min: 1,
+      max: 100,
+      slide: function (event, ui) {
+        $("#amount").val("$" + ui.value);
+      }
     });
+    $("#amount").val("$" + $("#slider-range-min").slider("value"));
+  });
 
-// установливаем обработчик события resize
-    $(window).resize(function () {
-        $(".header-full .container").css("height", $(window).height());
-    });
-
-// вызовем событие resize
-    $(window).resize();
+  $(".volume-control").click(function () {
+    $('.slider-range').toggleClass("slider-range-open");
+  });
 });
 
-$(function () {
-    //scroll to top
-    $('.to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 500);
-        return false;
-    });
 
-    //show up-button
-    $(document).scroll(function () {
-        var y = $(this).scrollTop();
-        if (y > 800) {
-            $('.to-top').removeClass('hidden');
-        } else {
-            $('.to-top').addClass('hidden');
-        }
-    });
-});
+/*
 
-//show up-button
-$(document).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > 800) {
-        $('.to-top').fadeIn();
-    } else {
-        $('.to-top').fadeOut();
-    }
-});
+ $(function () {
+ $("a[href^='#']").click(function () {
+ var _href = $(this).attr("href");
+ $("html, body").animate({scrollTop: $(_href).offset().top + "px"});
+ return false;
+ });
+ });
+
+ // установливаем обработчик события resize
+ $(window).resize(function () {
+ $(".header-full .container").css("height", $(window).height());
+ });
+
+ // вызовем событие resize
+ $(window).resize();
+
+ $('.my-background-video').bgVideo();
+
+
+ $(function () {
+ //scroll to top
+ $('.to-top').click(function () {
+ $('html, body').animate({scrollTop: 0}, 500);
+ return false;
+ });
+
+ //show up-button
+ $(document).scroll(function () {
+ var y = $(this).scrollTop();
+ if (y > 800) {
+ $('.to-top').removeClass('hidden');
+ } else {
+ $('.to-top').addClass('hidden');
+ }
+ });
+ });
+
+ //show up-button
+ $(document).scroll(function () {
+ var y = $(this).scrollTop();
+ if (y > 800) {
+ $('.to-top').fadeIn();
+ } else {
+ $('.to-top').fadeOut();
+ }
+ });
+ */
--- src/js/lib.js
+++ src/js/lib.js
@@ -2,5 +2,14 @@
 
 //= lib/smoothscroll.js
 
+//= lib/jquery-ui.min.js
+
+// = ../../bower_components/jquery-background-video/jquery.background-video.js
 // = lib/bootstrap.min.js
 // = lib/jquery.fancybox.min.js
+// = ../../bower_components/jquery-ui/ui/widget.js
+// = ../../bower_components/jquery-ui/ui/keycode.js
+// = ../../bower_components/jquery-ui/ui/effect.js
+// = ../../bower_components/jquery-ui/ui/widgets/mouse.js
+// = ../../bower_components/jquery-ui/ui/widgets/slider.js
+// = ../../bower_components/jquery-ui/ui/effects/effect-slide.js
