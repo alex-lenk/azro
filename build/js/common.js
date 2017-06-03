289e5b454d3cdaaf020bcb016ad9f5daf8d2ea1f
artlenk.ru@gmail.com <df5ukoi7ht>
2017-06-03 14:54:42 +0300

    Added effect navbar-toggle. Moved scripts from footer in contacts.html

--- build/js/common.js
+++ build/js/common.js
@@ -4,8 +4,20 @@ $(document).ready(function () {
   });
 
   $(".navbar-toggle").click(function () {
-    $('.navbar-toggle').toggleClass("navbar-toggle-open");
     $('.nav-panel').toggleClass("nav-panel-open");
+  });
+
+
+  var formControl = $(".form-control"),
+    formGroup = ".form-group",
+    formGroupFocus = "form-group-focus";
+
+  formControl.focus(function () {
+    $(this).closest(formGroup).addClass(formGroupFocus);
+  });
+
+  formControl.blur(function () {
+    $(this).closest(formGroup).removeClass(formGroupFocus);
   });
 
   $(function () {
@@ -62,383 +74,6 @@ $(document).ready(function () {
     }
   });
 });
-
-var colors = new Array(
-  [15, 92, 128],
-  [35, 37, 37]
-);
-
-var step = 0;
-//color table indices for:
-// current color left
-// next color left
-// current color right
-// next color right
-var colorIndices = [0, 1, 1, 0];
-
-//transition speed
-var gradientSpeed = 0.002;
-
-function updateGradient() {
-  if ($ === undefined) return;
-
-  var c0_0 = colors[colorIndices[0]];
-  var c0_1 = colors[colorIndices[1]];
-  var c1_0 = colors[colorIndices[2]];
-  var c1_1 = colors[colorIndices[3]];
-
-  var istep = 1 - step;
-  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
-  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
-  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
-  var color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
-
-  var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
-  var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
-  var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
-  var color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
-
-  $(".about-us-advantages-decor").css({
-    background: "-webkit-gradient(linear, left top, right top, from(" +
-    color1 +
-    "), to(" +
-    color2 +
-    "))"
-  }).css({
-    background: "-moz-linear-gradient(left, " + "-45deg, " +
-    color1 +
-    " 0%, " +
-    color2 +
-    " 100%)"
-  });
-
-  step += gradientSpeed;
-  if (step >= 1) {
-    step %= 1;
-    colorIndices[0] = colorIndices[1];
-    colorIndices[2] = colorIndices[3];
-
-    //pick two new target color indices
-    //do not pick the same as the current one
-    colorIndices[1] =
-      (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) %
-      colors.length;
-    colorIndices[3] =
-      (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) %
-      colors.length;
-  }
-}
-
-setInterval(updateGradient, 10);
-
-var mapGoogle = {
-  init: function () {
-    var $lati = $('#map-canvas').attr('data-lat'),
-      $longi = $('#map-canvas').attr('data-long'),
-      map = {};
-    map = new google.maps.Map(document.getElementById('map-canvas'), this.mapOptions($lati, $longi));
-    this.addMarker($lati, $longi, map);
-  },
-  addMarker: function ($lati, $longi, map) {
-    var egglabs = new google.maps.LatLng($lati, $longi),
-      markers = [],
-      image = new google.maps.MarkerImage(
-        'img/fake.gif',
-        new google.maps.Size(101, 82),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(42, 56)
-      );
-    markers.push(new google.maps.Marker({
-      position: egglabs,
-      raiseOnDrag: false,
-      icon: image,
-      map: map,
-      draggable: false
-    }));
-  },
-  mapOptions: function ($lati, $longi) {
-    var mapCoordinates = new google.maps.LatLng($lati, $longi);
-    var mapOptions = {
-      zoom: 15,
-      disableDefaultUI: false,
-      center: mapCoordinates,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-      scrollwheel: false,
-      styles: [
-        {
-          "featureType": "all",
-          "elementType": "labels",
-          "stylers": [
-            {
-              "visibility": "on"
-            }
-          ]
-        },
-        {
-          "featureType": "all",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "saturation": 36
-            },
-            {
-              "color": "#000000"
-            },
-            {
-              "lightness": 40
-            }
-          ]
-        },
-        {
-          "featureType": "all",
-          "elementType": "labels.text.stroke",
-          "stylers": [
-            {
-              "visibility": "on"
-            },
-            {
-              "color": "#000000"
-            },
-            {
-              "lightness": 16
-            }
-          ]
-        },
-        {
-          "featureType": "all",
-          "elementType": "labels.icon",
-          "stylers": [
-            {
-              "visibility": "off"
-            }
-          ]
-        },
-        {
-          "featureType": "administrative",
-          "elementType": "geometry.fill",
-          "stylers": [
-            {
-              "color": "#000000"
-            },
-            {
-              "lightness": 20
-            }
-          ]
-        },
-        {
-          "featureType": "administrative",
-          "elementType": "geometry.stroke",
-          "stylers": [
-            {
-              "color": "#000000"
-            },
-            {
-              "lightness": 17
-            },
-            {
-              "weight": 1.2
-            }
-          ]
-        },
-        {
-          "featureType": "administrative.locality",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#c4c4c4"
-            }
-          ]
-        },
-        {
-          "featureType": "administrative.neighborhood",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#707070"
-            }
-          ]
-        },
-        {
-          "featureType": "landscape",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#000000"
-            },
-            {
-              "lightness": 20
-            }
-          ]
-        },
-        {
-          "featureType": "poi",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#000000"
-            },
-            {
-              "lightness": 21
-            },
-            {
-              "visibility": "on"
-            }
-          ]
-        },
-        {
-          "featureType": "poi.business",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "visibility": "on"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry.fill",
-          "stylers": [
-            {
-              "color": "#33ddfb"
-            },
-            {
-              "lightness": "0"
-            },
-            {
-              "visibility": "on"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "geometry.stroke",
-          "stylers": [
-            {
-              "visibility": "off"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "visibility": "off"
-            }
-          ]
-        },
-        {
-          "featureType": "road.highway",
-          "elementType": "labels.text.stroke",
-          "stylers": [
-            {
-              "visibility": "off"
-            },
-            {
-              "hue": "#00d9ff"
-            }
-          ]
-        },
-        {
-          "featureType": "road.arterial",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#000000"
-            },
-            {
-              "lightness": 18
-            }
-          ]
-        },
-        {
-          "featureType": "road.arterial",
-          "elementType": "geometry.fill",
-          "stylers": [
-            {
-              "color": "#575757"
-            }
-          ]
-        },
-        {
-          "featureType": "road.arterial",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#ffffff"
-            }
-          ]
-        },
-        {
-          "featureType": "road.arterial",
-          "elementType": "labels.text.stroke",
-          "stylers": [
-            {
-              "color": "#2c2c2c"
-            }
-          ]
-        },
-        {
-          "featureType": "road.local",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#000000"
-            },
-            {
-              "lightness": 16
-            }
-          ]
-        },
-        {
-          "featureType": "road.local",
-          "elementType": "labels.text.fill",
-          "stylers": [
-            {
-              "color": "#999999"
-            }
-          ]
-        },
-        {
-          "featureType": "road.local",
-          "elementType": "labels.text.stroke",
-          "stylers": [
-            {
-              "saturation": "-52"
-            }
-          ]
-        },
-        {
-          "featureType": "transit",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#000000"
-            },
-            {
-              "lightness": 19
-            }
-          ]
-        },
-        {
-          "featureType": "water",
-          "elementType": "geometry",
-          "stylers": [
-            {
-              "color": "#000000"
-            },
-            {
-              "lightness": 17
-            }
-          ]
-        }
-      ]
-    };
-    return mapOptions;
-  }
-};
-
-mapGoogle.init();
 
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
@@ -625,3 +260,71 @@ mapGoogle.init();
 }));
 
 transformicons.add('.tcon');
+
+var colors = new Array(
+  [15, 92, 128],
+  [35, 37, 37]
+);
+
+var step = 0;
+//color table indices for:
+// current color left
+// next color left
+// current color right
+// next color right
+var colorIndices = [0, 1, 1, 0];
+
+//transition speed
+var gradientSpeed = 0.002;
+
+function updateGradient() {
+  if ($ === undefined) return;
+
+  var c0_0 = colors[colorIndices[0]];
+  var c0_1 = colors[colorIndices[1]];
+  var c1_0 = colors[colorIndices[2]];
+  var c1_1 = colors[colorIndices[3]];
+
+  var istep = 1 - step;
+  var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+  var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+  var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+  var color1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
+
+  var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+  var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+  var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+  var color2 = "rgb(" + r2 + "," + g2 + "," + b2 + ")";
+
+  $(".about-us-advantages-decor").css({
+    background: "-webkit-gradient(linear, left top, right top, from(" +
+    color1 +
+    "), to(" +
+    color2 +
+    "))"
+  }).css({
+    background: "-moz-linear-gradient(left, " + "-45deg, " +
+    color1 +
+    " 0%, " +
+    color2 +
+    " 100%)"
+  });
+
+  step += gradientSpeed;
+  if (step >= 1) {
+    step %= 1;
+    colorIndices[0] = colorIndices[1];
+    colorIndices[2] = colorIndices[3];
+
+    //pick two new target color indices
+    //do not pick the same as the current one
+    colorIndices[1] =
+      (colorIndices[1] + Math.floor(1 + Math.random() * (colors.length - 1))) %
+      colors.length;
+    colorIndices[3] =
+      (colorIndices[3] + Math.floor(1 + Math.random() * (colors.length - 1))) %
+      colors.length;
+  }
+}
+
+setInterval(updateGradient, 10);
